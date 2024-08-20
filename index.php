c53093c24577f810936e3f8d197372700c76e594
guilherme <guilherme@acheipneus.com.br>
2024-08-20 00:08:32 -0300

    same php foreach loop for lunch products

--- index.php
+++ index.php
@@ -26,6 +26,32 @@ $produtosCafe = [
         'imagem' => "img/cafe-gelado.jpg"
     ],
 ];
+$produtosAlmoco = [
+    [
+        "nome" => "Bife",
+        "descricao" => "Bife, arroz com feijão e uma deliciosa batata frita",
+        "preco" => "27.90",
+        "imagem" => "img/bife.jpg"
+    ],
+    [
+        "nome" => "Filé de peixe",
+        "descricao" => "Filé de peixe salmão assado, arroz, feijão verde e tomate.",
+        "preco" => "24.99",
+        "imagem" => "img/prato-peixe.jpg"
+    ],
+    [
+        "nome" => "Frango",
+        "descricao" => "Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante",
+        "preco" => "23.00",
+        "imagem" => "img/prato-frango.jpg"
+    ],
+    [
+        "nome" => "Fettuccine",
+        "descricao" => "Prato italiano autêntico da massa do fettuccine com peito de frango grelhado",
+        "preco" => "22.50",
+        "imagem" => "img/fettuccine.jpg"
+    ]
+];
 ?>
 <head>
     <meta charset="UTF-8">
@@ -73,38 +99,16 @@ $produtosCafe = [
                 <img class= "ornaments" src="img/ornaments-coffee.png" alt="ornaments">
             </div>
             <div class="container-almoco-produtos">
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/bife.jpg">
+                <?php foreach ($produtosAlmoco as $almoco):?>
+                    <div class="container-produto">
+                        <div class="container-foto">
+                            <img src="<?= $almoco['imagem'] ?>">
+                        </div>
+                        <p><?= $almoco['nome'] ?></p>
+                        <p><?= $almoco['descricao'] ?></p>
+                        <p><?= "R$ " . $almoco['preco'] ?></p>
                     </div>
-                    <p>Bife</p>
-                    <p>Bife, arroz com feijão e uma deliciosa batata frita</p>
-                    <p>R$ 27.90</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/prato-peixe.jpg">
-                    </div>
-                    <p>Filé de peixe</p>
-                    <p>Filé de peixe salmão assado, arroz, feijão verde e tomate.</p>
-                    <p>R$ 24.99</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/prato-frango.jpg">
-                    </div>
-                    <p>Frango</p>
-                    <p>Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante</p>
-                    <p>R$ 23.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/fettuccine.jpg">
-                    </div>
-                    <p>Fettuccine</p>
-                    <p>Prato italiano autêntico da massa do fettuccine com peito de frango grelhado</p>
-                    <p>R$ 22.50</p>
-                </div>
+                <?php endforeach; ?>
             </div>
 
         </section>
